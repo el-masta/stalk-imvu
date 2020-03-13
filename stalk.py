a8b69fca7f44f559a47edc0607fd0ebe81e5d5b0
Mario Rodríguez <62149205+el-masta@users.noreply.github.com>
2020-03-13 11:01:22 -0600

    Add files via upload

--- stalk.py
+++ stalk.py
@@ -9,83 +9,85 @@ from pymongo import MongoClient
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import concurrent.futures
 
 client = MongoClient('localhost', 27017)
 db = client.imvu
-seguidores = db.seguidores
-publicaciones = db.publicaciones
+seguidores = db.cseguidores
+publicaciones = db.cpublicaciones
 
-susodicho='' # Id de usuario a stalkear
-
+susodicho='' # ID de usuario IMVU a stalkear
 
 headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36"}
-cookies = {''} # Aqui se arma el objeto de cookies
+cookies = {''} # Aqui se arma el diccionario de cookies
 
-def paso1(target):
-   #Encuentra seguidores
+def encontrarSeguidores(target):
+   '''Encuentra seguidores'''
    url='https://api.imvu.com/profile/profile-user-'+target+'/subscriptions?limit=1500'
    respuesta=json.loads(requests.get(url).text)
-   #for user in respuesta['denormalized'][url]['data']['items']:
    for user in respuesta['denormalized'][url]['data']['items']:
       seguidorid=user[79:]
-      print ('Seguido:\t'+seguidorid+ ' agregado')
-      seguidores.update_one({'_id':seguidorid},{'$set':{'_id':seguidorid}}, upsert=True )
+      res=seguidores.find_one({ '_id' : seguidorid },{'nombre':1})
+      if res!=None:
+         print('Usuario saltado ya revisado')
+      else:
+         seguidores.update_one({'_id':seguidorid},{'$set':{'_id':seguidorid,'rank':0}}, upsert=True )
+         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor1:
+            executor1.map( guardarNombre(seguidorid) )
+            executor1.map( encontrarPosts(seguidorid) )
+         print ('Seguido:\t'+seguidorid+ ' agregado')
+      res= None
 
-def paso2():
-   #Encuentra posts de los seguidores y los guarda en publicaciones
 
-   # Recupera seguidores
-   followers=seguidores.find({},{'_id':1})
-   a=0
+def guardarNombre(user):
+   '''Guarda nombre de usuario'''
+   url='https://api.imvu.com/users/cid/'+user
+   respuesta=json.loads(requests.get( url, headers=headers, cookies=cookies).content)
+   nombre=respuesta['denormalized'][url]['data']['avatarname']
+   print ('Usuario:'+nombre )
+   seguidores.update_one({'_id':user},{'$set':{'nombre':nombre}}, upsert=True )
+
+def encontrarPosts(target):
+   '''Encuentra los posts'''
+   url='https://api.imvu.com/feed/feed-personal-'+target+'?limit=40'
+   normalized='https://api.imvu.com/feed/feed-personal-'+target+'/elements'
+   respuesta=json.loads(requests.get( url, headers=headers, cookies=cookies).text)
    b=0
-   for follower in followers:
-      target=follower['_id']
-      a+=1
-      guardarNombre(target)
+   print( str(len(respuesta['denormalized'][normalized]['data']['items']))+' posts' )
+   for post in respuesta['denormalized'][normalized]['data']['items']:
+      b+=1
+      postid=post[72:]
+      print ('Post ('+str(b)+'):\t'+postid)
+      explorarPosts(postid,target)
 
-      #guarda id de publicacion
-      url='https://api.imvu.com/feed/feed-personal-'+target+'?limit=40'
-      normalized='https://api.imvu.com/feed/feed-personal-'+target+'/elements'
-      respuesta=json.loads(requests.get( url, headers=headers, cookies=cookies).text)
-      for post in respuesta['denormalized'][normalized]['data']['items']:
-         b+=1
-         postid=post[72:]
-         print ('Post ('+str(a)+'-'+str(b)+'):\t'+postid)
-         publicaciones.update_one({'_id':postid},{'$set':{'_id':postid, 'user':target }}, upsert=True )
-
-def paso3():
-   # Recupera publicaciones
-   posts=publicaciones.find({ 'checked' : {'$nin':[True] } },{'_id':1})
-   a=0
-   for post in posts:
-      a+=1
-      target=post['_id']
-      print (str(a)+':\t'+target)
-      #Encuentra likes y comentarios
-      encontrarLikes(target)
-      encontrarComentarios(target)
-      encontrarTags(target)
-      publicaciones.update_one({'_id':target},{'$set':{'checked':True}})
+def explorarPosts(postid,target):
+   '''Explora cada uno de los posts buscando likes, comentarios y etiquetas'''
+   with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor2:
+      executor2.map( encontrarLikes(postid,target) )
+      executor2.map( encontrarComentarios(postid,target) )
+      executor2.map( encontrarTags(postid,target) )
       
-def encontrarLikes(idPost):
+def encontrarLikes(idPost,target):
    url='https://api.imvu.com/feed_element/feed_element-'+idPost+'/liked_by/user-'+susodicho
    respuesta=json.loads(requests.get(url).text)
    if respuesta['status']=='success':
       print ('Like:\thttps://es.imvu.com/next/feed/feed_element-'+idPost+'/')
-      publicaciones.update_one({'_id':idPost},{'$set':{'liked':True}})
+      publicaciones.update_one({'_id':idPost},{'$set':{'_id':idPost,'liked':True,'user':target}}, upsert=True )
+      seguidores.update_one({'_id':target},{'$inc':{'rank':1}}, upsert=True )
          
-def encontrarComentarios(idPost):
+def encontrarComentarios(idPost,target):
    normalized='https://api.imvu.com/feed_element/feed_element-'+idPost+'/comments?limit=100'
    respuesta=json.loads(requests.get(normalized).text)
    if respuesta['status']=='failure':
-      publicaciones.delete_one({'_id':idPost})
+      pass
    else:
       for comment in respuesta['denormalized'][normalized]['data']['items']:
          if respuesta['denormalized']['https://api.imvu.com/feed_comment/feed_comment-'+comment[106:]]['relations']['author'][31:]==susodicho:
             print ('Coment:\thttps://es.imvu.com/next/feed/feed_element-'+idPost+'/')
-            publicaciones.update_one({'_id':idPost},{'$set':{'commented':True}})
+            publicaciones.update_one({'_id':idPost},{'$set':{'commented':True,'user':target}}, upsert=True )
+            seguidores.update_one({'_id':target},{'$inc':{'rank':1}}, upsert=True )
 
-def encontrarTags(idPost):
+def encontrarTags(idPost,target):
    urlPost='https://api.imvu.com/feed_element/feed_element-'+idPost
    respuesta=json.loads(requests.get(urlPost).text)
    if respuesta['status']=='success':
@@ -96,58 +98,31 @@ def encontrarTags(idPost):
             for tag in respuesta['denormalized']:
                if tag[31:]==susodicho:
                   print ('Tag:\thttps://es.imvu.com/next/feed/feed_element-'+idPost+'/')
-                  publicaciones.update_one({'_id':idPost},{'$set':{'tagged':True}})
-
-def guardarNombre(user):
-   target=user
-   #Guarda nombre de usuario
-   url='https://api.imvu.com/users/cid/'+target
-   #s = requests.Session()
-   respuesta=json.loads(requests.get( url, headers=headers, cookies=cookies).content)
-   nombre=respuesta['denormalized'][url]['data']['avatarname']
-   print ('Usuario:'+nombre )
-   seguidores.update_one({'_id':target},{'$set':{'nombre':nombre}}, upsert=True )
+                  publicaciones.update_one({'_id':idPost},{'$set':{'_id':idPost, 'tagged':True,'user':target}}, upsert=True )
+                  seguidores.update_one({'_id':target},{'$inc':{'rank':1}}, upsert=True )
 
 def exportarLigas():
-   f = open ('/home/mario/Escritorio/ligas.txt','w')
-   comentarios=publicaciones.find({ 'commented' : True },{'_id':1, 'user':1} ).sort( 'user',pymongo.ASCENDING )
-   likes=publicaciones.find({ 'liked' : True },{'_id':1,'user':1}).sort( 'user',pymongo.ASCENDING )
-   tags=publicaciones.find({ 'tagged' : True },{'_id':1,'user':1}).sort( 'user',pymongo.ASCENDING )
-   f.write('**** COMENTARIOS ****\n')
-   user=''
-   for comentario in comentarios:
-      if user!=comentario['user']:
-         user=comentario['user']
-         nombre=seguidores.find_one({ '_id' : user },{'nombre':1})['nombre']
-         f.write('\nA:\t'+nombre+' ('+user+')\n')
-      url='https://es.imvu.com/next/feed/feed_element-'+comentario['_id']+'/'
-      f.write(url+'\n')
-      print('Comentario:\t'+url)
-   f.write('\n\n**** LIKES ****\n')
-   user=''
-   for like in likes:
-      if user!=like['user']:
-         user=like['user']
-         nombre=seguidores.find_one({ '_id' : user },{'nombre':1})['nombre']
-         f.write('\nA:\t'+nombre+' ('+user+')\n')
-      url='https://es.imvu.com/next/feed/feed_element-'+like['_id']+'/'
-      f.write(url+'\n')
-      print('Like:\t'+url)
-   f.write('\n\n**** ETIQUETAS ****\n')
-   user=''
-   for tag in tags:
-      if user!=tag['user']:
-         user=tag['user']
-         nombre=seguidores.find_one({ '_id' : user },{'nombre':1})['nombre']
-         f.write('\nEn foto de:\t'+nombre+' ('+user+')\n')
-      url='https://es.imvu.com/next/feed/feed_element-'+tag['_id']+'/'
-      f.write(url+'\n')
-      print('Tag:\t'+url)
+   f = open ('~/Escritorio/ligas.txt','w')
+   followers=seguidores.find({'rank':{'$gt':0}},{'_id':1,'nombre':1}).sort( 'rank',pymongo.DESCENDING )
+   for seguidor in followers:
+      print('\nCon usuario: '+seguidor['nombre'])
+      f.write('\nCon usuario: '+seguidor['nombre']+'\n')
+      pubs=publicaciones.find({ 'user' : seguidor['_id'] }).sort( 'user',pymongo.ASCENDING )
+      for pub in pubs:
+         linea='https://es.imvu.com/next/feed/feed_element-'+pub['_id']+'/    '
+         if 'liked' in pub:
+            linea+='Like '
+         else:
+            linea+='     '
+         if 'commented' in pub:
+            linea+='Comentario '
+         else:
+            linea+='           '
+         if 'tagged' in pub:
+            linea+='Etiqueta '
+         print(linea)
+         f.write(linea+'\n')
    f.close()
-      
-paso1(susodicho)
-paso2()
-paso3()
+   
+encontrarSeguidores(susodicho)
 exportarLigas()
-
-
